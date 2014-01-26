708b58d40048d4ab7ce522fb2320a847903d03d5
Kristoffer Mendoza <kristoffer_mlmendoza@yahoo.com>
2014-01-27 05:25:02 +0800

    Redoing and improving homepage; recycling assets

--- application/views/home.php
+++ application/views/home.php
@@ -1,18 +1,160 @@
-<section id="jumbotron">
-
+<section class="jumbotron">
+<div class="row full">
+	<div class="large-6 columns">
+		<div class="row vcenter20 show-for-large-up"></div>
+		<div class="row vcenter10 hide-for-large-up"></div>
+		<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="100" height="100" viewBox="0 0 100 100">
+			<circle cx="5" cy="5" r="5" fill="white">
+				<animate attributeName="cx" from="5" to="95" begin="0s" dur="6s" values="5;5;95;95"
+					keySplines="
+            0 0 0 0; 
+            0.1 0.8 0.2 1;  
+            0 0 0 0"
+            keyTimes="0;0.2;0.8;1" calcMode="spline" repeatCount="indefinite" />
+			</circle>
+      <circle cx="95" cy="5" r="5" fill="white">
+      	<animate attributeName="cy" from="5" to="95" begin="0s" dur="6s" values="5;5;95;95"
+      		keySplines="
+            0 0 0 0; 
+            0.1 0.8 0.2 1;  
+            0 0 0 0"
+            keyTimes="0;0.2;0.8;1" calcMode="spline" repeatCount="indefinite" />
+      </circle>
+      <circle cx="5" cy="95" r="5" fill="white">
+      	<animate attributeName="cy" from="95" to="5" begin="0s" dur="6s" values="95;95;5;5"
+      		keySplines="
+            0 0 0 0; 
+            0.1 0.8 0.2 1;  
+            0 0 0 0"
+            keyTimes="0;0.2;0.8;1" calcMode="spline" repeatCount="indefinite" />
+      </circle>
+      <circle cx="95" cy="95" r="5" fill="white">
+      	<animate attributeName="cx" from="95" to="5" begin="0s" dur="6s" values="95;95;5;5"
+      		keySplines="
+            0 0 0 0; 
+            0.1 0.8 0.2 1;  
+            0 0 0 0"
+            keyTimes="0;0.2;0.8;1" calcMode="spline" repeatCount="indefinite" />
+      </circle>
+		</svg>
+	</div>
+	<div class="large-6 columns">
+		<div class="row vcenter40 show-for-large-up"></div>
+		<div class="row vcenter10 hide-for-large-up"></div>
+		<div style="background-color:rgba(52, 73, 94,0.5);padding:0px 10px 10px">
+			<h1 style="color:#f1c40f;margin-bottom:0px">Whatever you want it to be.</h1>
+			<h3 style="color:#e67e22">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy.</h3>
+			<p class="white justify" style="margin-bottom:0px">Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
+		</div>
+		<div class="row vcenter10 hide-for-large-up"></div>
+		</div>
+	</div>
+</div>
 </section>
 
-
-<section>
-
+<section class="homesection" id="description">
+	<div class="titleholder">
+		<div class="row full">
+			<div class="large-12 columns">
+				<h2 class="white">What is <span style="color:#f1c40f">Build</span><span style="color:#e67e22">Ex</span>?</h2>
+			</div>
+		</div>
+	</div>
+	<div class="row full">
+		<div class="large-5 columns"></div>
+		<div class="large-7 columns">
+			<h3 style="color:#f1c40f">Take full control of your experiments.</h3>
+			<p class="white justify">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
+		</div>
+	</div>
 </section>
 
+<section class="homesection" id="features">
+	<div class="titleholder">
+		<div class="row full">
+			<div class="large-12 columns">
+				<h2 class="white">Features</h2>
+			</div>
+		</div>
+	</div>
+	<div class="row full">
 
-<section>
-
+		<div class="large-3 columns">
+			<div class="row">
+				<div class="large-12 medium-6 small-12 columns">
+				</div>
+				<div class="large-12 medium-6 small-12 columns">
+					<h3 style="color:#f1c40f">All in one place</h3>
+					<p class="white">Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum.</p>
+				</div>
+			</div>
+		</div>
+		<div class="large-3 columns">
+			<div class="row">
+				<div class="large-12 medium-6 small-12 columns">
+				</div>
+				<div class="large-12 medium-6 small-12 columns">
+					<h3 style="color:#e67e22">User-friendly interface</h3>
+					<p class="white">Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum.</p>
+				</div>
+			</div>
+		</div>
+		<div class="large-3 columns">
+			<div class="row">
+				<div class="large-12 medium-6 small-12 columns">
+				</div>
+				<div class="large-12 medium-6 small-12 columns">
+					<h3 style="color:#f1c40f">Have it your way</h3>
+					<p class="white">Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem.</p>
+				</div>
+			</div>
+		</div>
+		<div class="large-3 columns">
+			<div class="row">
+				<div class="large-12 medium-6 small-12 columns">
+				</div>
+				<div class="large-12 medium-6 small-12 columns">
+					<h3 style="color:#e67e22">Connect with friends</h3>
+					<p class="white">Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius.</p>
+				</div>
+			</div>
+		</div>
+	</div>
 </section>
 
+<section class="homesection" id="showcase">
+	<div class="titleholder">
+		<div class="row full">
+			<div class="large-12 columns">
+				<h2 class="white">See It in Action</h2>
+			</div>
+		</div>
+	</div>
+	<div class="row full">
+		<div class=""></div>
+		<div class=""></div>
+	</div>
+</section>
 
-<section>
-
+<section class="homesection" id="about">
+	<div class="titleholder">
+		<div class="row full">
+			<div class="large-12 columns">
+				<h2 class="white">About <span style="color:#f1c40f">Build</span><span style="color:#e67e22">Ex</span></h2>
+			</div>
+		</div>
+	</div>
+	<div class="row full">
+		<div class="large-12 columns">
+			<div class="row">
+				<div class="large-3 columns"></div>
+				<div class="large-3 push-6 columns"></div>
+				<div class="large-6 pull-3 columns">
+					<p class="white">
+						Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
 </section>
