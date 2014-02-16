3005a7b3df333c64afe8ad176c31e39b85c23bd8
Neil <nmcalabroso@up.edu.ph>
2014-02-17 04:32:53 +0800

    Fixes the admin's graduate module.

--- application/views/admin/graduates.php
+++ application/views/admin/graduates.php
@@ -1,11 +1,13 @@
-	<h3> Admin: Graduates </h3>
+<h3> Admin: Graduates </h3>
 <hr>
-<?php 
-	$count = 0;
-	foreach ($graduates as $graduate){
-		$count = $count + 1;
-		echo $count.'. ';
-		echo anchor('graduate/view/'.$graduate->username, $graduate->username);
-		echo '</br>';
-	}
-?>
+<?php if(isset($graduates)): ?>
+	<?php $count = 0; ?>
+	<?php foreach ($graduates as $graduate):?>
+		<?php echo $count = $count + 1;?>
+		<p> <?php echo $count.'. '; ?>
+			<?php echo anchor('graduate/view/'.$graduate->username, $graduate->username); ?>
+		</p>
+	<? endforeach; ?>	
+<? else: ?>
+	<p> There are no graduates. </p>
+<? endif; ?>
