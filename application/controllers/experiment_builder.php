da2f3c2456d4718dde6afc7b6d2c04ee2b56c683
Mireya Andres <mireyagenandres@gmail.com>
2014-02-19 16:17:58 +0800

    Add builder_model and modify experiment_builder controller. Can add/update pages, forms, and options in the database. Database structure to be modified first for functions to work.

--- application/controllers/experiment_builder.php
+++ application/controllers/experiment_builder.php
@@ -8,4 +8,129 @@ class Experiment_builder extends MY_Controller{
 	public function app($eid = 0){
 		echo 'I am your builder.';
 	}
+
+	public function add_page(){
+		/*
+			Adds page. Returns pid. Required data listed below:
+				eid = experiment id
+				order = page number of current experiment
+				template = integer; form maker (1), text (2), comparison stimulus(3)
+
+			Additional data needed for grid (applies only if template is text or images):
+				row
+				column
+		*/
+
+		$data['eid'] = ;
+		$data['order'] = ;
+		$data['template'] = ;
+
+		if ($data['template'] > 1) {
+			$data['row'] = ;
+			$data['column'] = ;
+		}
+
+		$this->load->model('builder_model');
+		$this->builder_model->add_page($data);
+	}
+
+	public function add_form(){
+		/* 
+			Adds form/question. Returns qid. Required data listed below:
+				pid = page id
+				label = question itself (i.e. "Are you a psychopath?")
+				type = integer; text box (0), radio button (1), multiple choice (2), dropdown (3)
+				is_required = boolean; must be 't' or 'f'
+		*/
+
+		$data['pid'] = ;
+		$data['label'] = ;
+		$data['type'] = ;
+		$data['is_required'] = ;
+
+		$this->load->model('builder_model');
+		return $this->builder_model->add_form($data);
+	}
+
+	public function add_option(){
+		/*
+			Adds option. Applies only if form_type > 0 (builder_model checks this). Returns oid.
+			Required data listed below:
+				qid = id of form
+				label = option itself (i.e. yes, no, maybe)
+
+		*/
+
+		$data['qid'] = ;
+		$data['label'] = ;
+
+		$this->load->model('builder_model');
+		return $this->builder_model->add_option($data);
+	}
+
+	public function update_form(){
+		/*
+			Updates form/question; does not change order of questions. Required data listed below:
+				qid = id of form
+
+			Optional data to be updated:
+				label = change question
+				is_required = boolean; must be 't' or 'f'
+		*/
+
+		$data['qid'] = ;
+
+		$data['label'] = ;
+		$data['is_required'] = ;
+
+		$this->load->model('builder_model');
+		$this->builder_model->update_form($data);
+	}
+
+	public function update_option(){
+		/*
+			Updates individual options. Required data listed below:
+				oid = id of option
+				label = option itself
+		*/
+
+		$data['oid'] = ;
+		$data['label'] = ;
+
+		$this->load->model('builder_model');
+		$this->builder_model->update_option($data);
+	}
+
+	public function delete_page(){
+		/*
+			NOT YET DONE. DO NOT EDIT.
+
+			Deletes page along with associated form and options. Changes numbering accordingly.
+			Required data listed below:
+				eid = id of current experiment
+				pid = id of page
+		*/
+
+		// $data['eid'] = ;
+		// $data['pid'] = ;
+		$this->load->model('builder_model');
+		$this->builder_model->delete_page($data);
+	}
+
+	public function insert_page(){
+		/*
+			NOT YET DONE. DO NOT EDIT.
+
+			Inserts page in between and changes numbering accordingly. Required data listed below:
+				pid = id of page
+		*/
+
+		// $data['pid'] = ;
+		$this->load->model('builder_model');
+		$this->builder_model->insert_page($data);
+
+	}
+
+
+
 }
---
+++ application/models/builder_model.php
@@ -0,0 +1,82 @@
+<?php
+
+class Builder_model extends MY_Model{
+
+	function __construct(){
+		parent::__construct();
+	}
+
+	function add_page($data){
+		$this->db->insert('Pages',$data);
+		return $this->db->insert_id();
+	}
+
+	function add_form($data){
+		$this->db->insert('Questions',$data);
+		return $this->db->insert_id();
+	}
+
+	function add_option($data){
+		$this->db->where('qid', $data['qid']);
+		$query = $this->db->get('Questions');
+		$question = $query->row();
+		$type = $question->type;
+		echo $type;
+
+		if($type > 0){
+			$this->db->insert('Options',$data);
+			return $this->db->insert_id();
+		}
+	}
+
+	function update_form($data){
+		$this->db->where('qid', $data['qid']);
+		$this->db->update('Questions',$data);
+	}
+
+	function update_option($data){
+		$this->db->where('oid', $data['oid']);
+		$this->db->update('Options',$data);
+	}
+
+/*
+	------------------------------------------------------------------------------------
+	DO NOT EDIT SECTION BELOW
+	------------------------------------------------------------------------------------
+*/
+
+	function delete_page($data){
+		//gets order (for changing numbering) and qid (for deleting associated forms and options)
+		$this->db->where('pid', $data['pid']);
+		$query = $this->db->get('Pages');
+		$page = $query->row();
+		$order = $page->order;
+
+		$this->db->where('pid', $data['pid']);
+		$query = $this->db->get('Questions');
+		$form = $query->row();
+		$qid = $form->qid;
+
+		//deletes page and associated forms and options
+		$this->db->where('pid', $data['pid']);
+		$this->db->delete('Pages');
+
+		$this->db->where('pid', $data['pid']);
+		$this->db->delete('Questions');
+
+		$this->db->where('qid', $data['qid']);
+		$this->db->delete('Options');
+
+		//change numbering
+		$this->change_numbering($data['eid'], $order, -1);
+	}
+
+	function insert_page($data){
+	}
+
+	function change_numbering($eid, $order, $incr){
+	}
+
+
+
+}
