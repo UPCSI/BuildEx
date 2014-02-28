c7b123850986375723291a9b7689633726014822
Neil <nmcalabroso@up.edu.ph>
2014-03-01 06:42:34 +0800

    Reconfigures the form maker for faculty.

--- js/maker/app.js
+++ js/maker/app.js
@@ -3,14 +3,13 @@ define([
        "collections/snippets" , "collections/my-form-snippets",
        "views/tab" , "views/my-form",
        "text!data/input.json", "text!data/radio.json",
-       "text!data/select.json", "text!data/buttons.json","text!templates/app/render.html",  "text!templates/app/about.html",
+       "text!data/select.json", "text!templates/app/help.html",
 ], function(
   $, _, Backbone,
   SnippetsCollection, MyFormSnippetsCollection,
   TabView,
   MyFormView,
-  inputJSON, radioJSON, selectJSON, buttonsJSON,
-  renderTab, aboutTab
+  inputJSON, radioJSON, selectJSON, helpTab
 ){
   return {
     initialize: function(){
@@ -29,16 +28,8 @@ define([
         collection: new SnippetsCollection(JSON.parse(selectJSON))
       });
       new TabView({
-        title: "Buttons",
-        collection: new SnippetsCollection(JSON.parse(buttonsJSON))
-      });
-      new TabView({
-        title: "Rendered",
-        content: renderTab
-      });
-      new TabView({
-        title: "About",
-        content: aboutTab
+        title: "Help",
+        content: helpTab
       });
 
       //Make the first tab active!
