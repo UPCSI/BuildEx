f89762920d26d38f17449500c35422d1bad50a5f
nmcalabroso <=>
2014-04-03 13:36:08 +0800

    Reconfigure schema for initial draft.

--- application/controllers/respond.php
+++ application/controllers/respond.php
@@ -15,11 +15,25 @@ class Respond extends CI_Controller{
 			echo "Error 404: Page not found"; //handle this error
 			return 0;
 		}
+
 		$data['title'] = 'Respond';
 		$data['exp'] = $exp;
 		$data['var'] = $this->get_objects($exp->eid);
 		$data['main_content'] = 'builder/respondent/view';
 		$this->load->view('builder/respondent/_view_layout', $data);
+	}
+
+	public function save(){
+		$eid = $this->input_post('eid');
+		/*
+		save the answers here
+		*/
+		echo "Save success!";
+	}
+
+	public function pause(){
+		$eid = $this->input_post('eid');
+		/* not a priority */
 	}
 
 	private function get_objects($eid = 0){
