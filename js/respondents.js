8d6684f56d45ab821b937faf2b67e093e5e6b382
gardevior411 <earle.bunao@gmail.com>
2014-04-05 11:13:26 +0800

    for slideshow

--- js/respondents.js
+++ js/respondents.js
@@ -5,146 +5,141 @@ function clear_form(){
 	my_form.reset();
 }
 
+function draw_question(posX, posY, text_input, page_num){
+	posX = typeof posX !== 'undefined' ? posX : null;
+	posY = typeof posY !== 'undefined' ? posY : null;
+	page_num = typeof page_num !== 'undefined' ? page_num : 0;
+	text_input = typeof text_input !== 'undefined' ? text_input : "";
+	
+	var htmlData='<div id="qtn'+$.count+'" class = "static_obj" data-page=" ' + $.page + '"';
+
+	if (posX !== null && posY !== null){
+		htmlData += 'style="left:'+ posX +'px; top:'+ posY +'px;"';
+	}
+
+	if(text_input !== "")
+		htmlData += '><div id="qtneditable'+$.count+'" class="text-holder">'+text_input+'</div></div>';
+	else
+		htmlData += '><div id="qtneditable'+$.count+'" class="text-holder" data-placeholder="Enter Question" ></div></div>';
+
+	var temp = $.count;
+	var index = page_num;
+	if(index <= 0){
+		$("#page" + $.current_page).append(htmlData);
+	}
+	else{
+		$("#page" + index).append(htmlData);
+	}
+
+	$.count++;
+}
+
+function draw_text_input(posX, posY, text_input, page_num){
+	posX = typeof posX !== 'undefined' ? posX : null;
+	posY = typeof posY !== 'undefined' ? posY : null;
+	page_num = typeof page_num !== 'undefined' ? page_num : 0;
+	text_input = typeof text_input !== 'undefined' ? text_input : "";
+
+	var htmlData='<div id="inp'+$.count+'" class = "static_obj" data-page="' + $.page + '"';
+
+	if (posX !== null && posY !== null){
+		htmlData += 'style="left:'+ posX +'px; top:'+ posY +'px;""';
+	}
+	
+	if(text_input !== ""){
+		htmlData += '><div id="inpeditable'+$.count+'" class="text-holder">'+text_input+'</div></div>';
+	}
+	else{
+		htmlData += '><div id="inpeditable'+$.count+'"class="text-holder" data-placeholder="Enter Input" ></div></div>';
+	}
+	var temp = $.count;
+	var index = page_num;
+	if(index <= 0){
+		$("#page" + $.current_page).append(htmlData);
+	}
+	else{
+		$("#page" + index).append(htmlData);
+	}
+	$.count++;
+}
+
+function draw_button(posX, posY, text_input, page_num){
+	posX = typeof posX !== 'undefined' ? posX : null;
+	posY = typeof posY !== 'undefined' ? posY : null;
+	page_num = typeof page_num !== 'undefined' ? page_num : 0;
+	text_input = typeof text_input !== 'undefined' ? text_input : "Button";
+
+	alert(posX+","+posY);
+
+
+	var htmlData='<div id="btn'+$.count+'" class="static_obj" ' + 'data-page="' + $.page + '" ';
+
+	if (posX !== null && posY !== null){
+		htmlData += 'style="left:'+ posX +'px; top:'+ posY +'px;"';
+	}
+	else{
+		htmlData += 'style="width:150px; height:60"';
+	}
+	
+	htmlData += 'style="width:150px; height:60"><button id="btneditable'+$.count+'" style="width:100%; height:100%; margin-bottom:0px; padding:0px">'+text_input+'</button></div>';
+	
+	var temp = $.count;
+
+	var index = page_num;
+
+	if(index <= 0)
+		$("#page" + $.current_page).append(htmlData);
+	else
+		$("#page" + index).append(htmlData);
+
+	$(document).click(function(e){
+		if($(e.target).attr('id') == ('btneditable'+temp)){
+			$(e.target).children().click();
+			$(e.target).children().focus();
+		}
+	});
+	$.count++;
+}
+
+function draw_radio_button(posX, posY, page_num){
+	posX = typeof posX !== 'undefined' ? posX : null;
+	posY = typeof posY !== 'undefined' ? posY : null;
+	page_num = typeof page_num !== 'undefined' ? page_num : 0;
+
+	var htmlData='<div id="radbtn'+$.count+'" class="radiosnap static_obj" ' + 'data-page="' + $.page + '" ';
+	if (posX !== null && posY !== null){
+		htmlData += 'style="left:'+ posX +'px; top:'+ posY +'px; height:25px; width:120px;"';
+	}
+	else{
+		htmlData += 'style="height:25px; width:120px;"';
+	}
+	
+	// faulty -- contentEditable=true data-ph="My Placeholder String"
+	htmlData += '><input type="radio" id="radeditable'+$.count+'" name="'+$.page+'" value="radiobutton"><div class="default" style="width:100%; height:100%; display:inline; vertical-align:middle">Radio Button</div></div>';
+	
+	var temp = $.count;
+	var index = page_num;
+
+	if(index <= 0)
+		$("#page" + $.current_page).append(htmlData);
+	else
+		$("#page" + index).append(htmlData);
+
+	$(document).click(function(e){
+		if($(e.target).attr('id') == ('radbtn'+temp)){
+			$(e.target).children('.default').click();
+			$(e.target).children('.default').focus();
+		}
+	});
+}
+
+
+
 (function($){
 $(function() {
 	$.count = 1;
 	$.page = 1;
 	$.current_page = 1;
-	
-	function draw_question(posX, posY, text_input, page_num){
-		posX = typeof posX !== 'undefined' ? posX : null;
-		posY = typeof posY !== 'undefined' ? posY : null;
-		page_num = typeof page_num !== 'undefined' ? page_num : 0;
-		text_input = typeof text_input !== 'undefined' ? text_input : "";
-		
-		var htmlData='<div id="qtn'+$.count+'" class="ui-widget-content" ' + 'data-page="' + $.page + '"';
-
-		if (posX !== null && posY !== null){
-			htmlData += 'style="left:'+ posX +'px; top:'+ posY +'px;""';
-		}
-
-		if(text_input !== "")
-			htmlData += '><a href="#" class="delete"></a><div id="qtneditable'+$.count+'" class="editable">'+text_input+'</div></div>';
-		else
-			htmlData += '><a href="#" class="delete"></a><div id="qtneditable'+$.count+'" class="editable" data-placeholder="Enter Question" ></div></div>';
-
-		var temp = $.count;
-		var index = page_num;
-		if(index <= 0){
-			$("#page" + $.current_page).append(htmlData);
-		}
-		else{
-			$("#page" + index).append(htmlData);
-		}
-    }
-
-	$('#textinput')
-		.click(function(eventClick, posX, posY, text_input, page_num){
-			posX = typeof posX !== 'undefined' ? posX : null;
-			posY = typeof posY !== 'undefined' ? posY : null;
-			page_num = typeof page_num !== 'undefined' ? page_num : 0;
-			text_input = typeof text_input !== 'undefined' ? text_input : "";
-			
-			var htmlData='<div id="inp'+$.count+'" class="ui-widget-content" ' + 'data-page="' + $.page + '"';
-
-			if (posX !== null && posY !== null){
-				// alert('x' + posX);
-				// alert('y' + posY);
-				htmlData += 'style="left:'+ posX +'px; top:'+ posY +'px;""';
-			}
-			
-			if(text_input !== ""){
-				htmlData += '><a href="#" class="delete"></a><div id="inpeditable'+$.count+'" class="editable">'+text_input+'</div></div>';
-			}
-			else{
-				htmlData += '><a href="#" class="delete"></a><div id="inpeditable'+$.count+'" class="editable" data-placeholder="Enter Input" ></div></div>';
-			}
-			var temp = $.count;
-			var index = page_num;
-			if(index <= 0)
-				$("#page" + $.current_page).append(htmlData);
-			else
-				$("#page" + index).append(htmlData);
-
-			$('#inpeditable'+temp).click(function(){
-				$(this).attr('contenteditable','true');
-			});
-    });
-
-	$('#button')
-		.click(function(eventClick, posX, posY, text_input, page_num){
-			posX = typeof posX !== 'undefined' ? posX : null;
-			posY = typeof posY !== 'undefined' ? posY : null;
-			page_num = typeof page_num !== 'undefined' ? page_num : 0;
-			text_input = typeof text_input !== 'undefined' ? text_input : "Button";
-
-			var htmlData='<div id="btn'+$.count+'" class="" ' + 'data-page="' + $.page + '" ';
-			if (posX !== null && posY !== null){
-				// alert('x' + posX);
-				// alert('y' + posY);
-				htmlData += 'style="left:'+ posX +'px; top:'+ posY +'px;"';
-			}
-			else{
-				htmlData += 'style="width:150px; height:60"';
-			}
-			
-			htmlData += 'style="width:150px; height:60"><button id="btneditable'+$.count+'" style="width:100%; height:100%; margin-bottom:0px; padding:0px"><div class="default" style="width:100%; height:100%; display:inline; vertical-align:middle">'+text_input+'</div></button><a href="#" class="delete"></a></div>';
-			
-			var temp = $.count;
-
-			var index = page_num;
-
-			if(index <= 0)
-				$("#page" + $.current_page).append(htmlData);
-			else
-				$("#page" + index).append(htmlData);
-
-			$('.default').click(function(){
-				$(this).attr('contenteditable','true');
-			});
-
-			$(document).click(function(e){
-				if($(e.target).attr('id') == ('btneditable'+temp)){
-					$(e.target).children().click();
-					$(e.target).children().focus();
-				}
-			});
-    });
-
-	$('#radiobutton')
-		.click(function(eventClick, posX, posY, page_numm){
-			posX = typeof posX !== 'undefined' ? posX : null;
-			posY = typeof posY !== 'undefined' ? posY : null;
-			page_num = typeof page_num !== 'undefined' ? page_num : 0;
-
-			var htmlData='<div id="radbtn'+$.count+'" class="radiosnap draggable ui-draggable" ' + 'data-page="' + $.page + '" ';
-			if (posX !== null && posY !== null){
-				// alert('x' + posX);
-				// alert('y' + posY);
-				htmlData += 'style="left:'+ posX +'px; top:'+ posY +'px; height:25px; width:120px;"';
-			}
-			else{
-				htmlData += 'style="height:25px; width:120px;"';
-			}
-			
-			// faulty -- contentEditable=true data-ph="My Placeholder String"
-			htmlData += '><input type="radio" id="radeditable'+$.count+'" name="'+$.page+'" value="radiobutton"><div class="default" style="width:100%; height:100%; display:inline; vertical-align:middle">Radio Button</div><a href="#" class="delete"></a></div>';
-			
-			var temp = $.count;
-			var index = page_num;
-
-			if(index <= 0)
-				$("#page" + $.current_page).append(htmlData);
-			else
-				$("#page" + index).append(htmlData);
-
-			$(document).click(function(e){
-				if($(e.target).attr('id') == ('radbtn'+temp)){
-					$(e.target).children('.default').click();
-					$(e.target).children('.default').focus();
-				}
-			});
-    });
 
 	$('#checkbox')
 		.click(function(eventClick, posX, posY, page_num){
