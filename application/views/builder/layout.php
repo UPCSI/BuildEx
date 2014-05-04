03e1a4acc1232cd28f7270cf4e2f7b12c86e2383
Earl <earl@MacBook-Pro.local>
2014-05-04 19:31:34 +0800

    Reposition of navigation of slides

--- application/views/builder/layout.php
+++ application/views/builder/layout.php
@@ -6,29 +6,37 @@ $this->load->view('builder/topbar');
 ?>
 
 <div class="row full legit" style="min-height:100%;height:100%">
-<div class="large-10 medium-10 small-10 columns unpad-h" style="min-height:100%;height:100%">
-<div class="row full legit" style="min-height:73%;height:73%">
-<div class="large-3 medium-3 small-3 column unmar-v" style="min-height:100%;height:100%;margin-top:10px;margin-bottom:10px;margin-top:0px;
+	<div class="large-10 medium-10 small-10 columns unpad-h" style="min-height:100%;height:100%">
+		<div class="row full legit" style="min-height:73%;height:73%">
+			<div class="large-3 medium-3 small-3 column unmar-v" style="min-height:100%;height:100%;margin-top:10px;margin-bottom:10px;margin-top:0px;
     margin-bottom:0px;">
-<div class="slides panel callout" style="min-height:100%;height:100%;overflow-y:auto;">
-
-<?php $this->load->view('builder/panels'); ?>
-
-</div>
-</div>
-<div class="large-9 medium-9 small-9 column" style="min-height:100%;height:100%;overflow:auto;display:flex;line-height:initial;text-align:-webkit-center;">
-<?php $this->load->view($main_content); ?>
-</div>
-</div>
-<div class="row full legit" style="min-height:27%;height:27%;margin-left:0px;margin-right:0px;background:#252525">
-<div class="large-12 medium-12 small-12 column unpad-h" style="top: -1px;">
-<?php $this->load->view('builder/elements'); ?>
-</div>
-</div>
-</div>
-<div class="large-2 medium-2 small-2 column" style="min-height:100%;height:100%;overflow-y:auto;background:#252525;padding:0px">
-<?php $this->load->view('builder/settings'); ?>
-</div>
+				<div class="slides panel callout" style="min-height:90%;height:90%;overflow-y:auto;margin-bottom:0px;">
+				</div>
+				<div class="panel callout" style="min-height:10%;height:10%;padding:0px;margin:0px">
+					<ul class="off-canvas-list">
+						<li><center>
+							<ul class="button-group" style="display:table;margin-top:10px;margin-bottom:10px">
+							  <li><a href="#" id="prevPage" class="button success tiny" style="display:inline-flex;margin:0"><i class="fa fa-arrow-left"></i></a></li>
+							  <li><a href="#" id="newPage" class="button success tiny" style="display:inline-flex;margin:0"><i class="fa fa-plus"></i></a></li>
+							  <li><a href="#" id="nextPage" class="button success tiny" style="display:inline-flex;margin:0"><i class="fa fa-arrow-right"></i></a></li>
+							</ul>
+						</center></li>
+					</ul>
+				</div>
+			</div>
+			<div class="large-9 medium-9 small-9 column" style="min-height:100%;height:100%;overflow:auto;display:flex;line-height:initial;text-align:-webkit-center;">
+				<?php $this->load->view($main_content); ?>
+			</div>
+		</div>
+		<div class="row full legit" style="min-height:27%;height:27%;margin-left:0px;margin-right:0px;background:#252525">
+			<div class="large-12 medium-12 small-12 column unpad-h" style="top: -1px;">
+				<?php $this->load->view('builder/elements'); ?>
+			</div>
+		</div>
+	</div>
+	<div class="large-2 medium-2 small-2 column" style="min-height:100%;height:100%;overflow-y:auto;background:#252525;padding:0px">
+		<?php $this->load->view('builder/settings'); ?>
+	</div>
 </div>
 <?php $this->load->view('builder/footer'); ?>
 
--- application/views/builder/settings.php
+++ application/views/builder/settings.php
@@ -1,11 +1,4 @@
 <ul class="off-canvas-list">
-	<li><center>
-		<ul class="button-group" style="display:table;margin-top:10px;margin-bottom:10px">
-		  <li><a href="#" id="prevPage" class="button success tiny" style="display:inline-flex;margin:0"><i class="fa fa-arrow-left"></i></a></li>
-		  <li><a href="#" id="newPage" class="button success tiny" style="display:inline-flex;margin:0"><i class="fa fa-plus"></i></a></li>
-		  <li><a href="#" id="nextPage" class="button success tiny" style="display:inline-flex;margin:0"><i class="fa fa-arrow-right"></i></a></li>
-		</ul>
-	</center></li>
 	<li><label><center><span style="color:#2ecc71">Object</span></center></label></li>
 	<li><label>Properties</label></li>
 	<li><a href = "#"> Type </a></li>
