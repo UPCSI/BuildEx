09751ab6af921268aa436dcc15a3ad48092838c3
nmcalabroso <nmcalabroso@up.edu.ph>
2014-07-18 07:26:07 +0800

    Fix minor issues.

--- application/views/faculty/_laboratory_summary.php
+++ application/views/faculty/_laboratory_summary.php
@@ -2,7 +2,7 @@
   <h1><?php echo $laboratory->name; ?></h1>
   <p><?php echo $laboratory->description; ?></p>
   <p>Laboratory Head: <?php echo anchor(faculty_path($laboratory_head), $laboratory_head->username); ?></p>
-  <p>Members: <?php echo '25'; ?></p>
+  <p>Members: <?php echo $laboratory->members_count; ?></p>
 <?php else: ?>
   <p>You have no laboratory yet. <?php echo anchor(site_url('explore'), 'Explore!'); ?></p>
 <?php endif; ?>
