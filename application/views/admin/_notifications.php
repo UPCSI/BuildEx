24ebaad5d00a7acbba01fe3ae7202d5f39a94355
nmcalabroso <nmcalabroso@up.edu.ph>
2014-07-18 04:50:01 +0800

    Changes admin homepage.

--- application/views/admin/_notifications.php
+++
@@ -1,3 +0,0 @@
-<h2> Notifications </h2>
-<h3> Users </h3>
-    <p> There are 20 newly registered users for this week. </p>
--- application/views/admin/home.php
+++ application/views/admin/home.php
@@ -1,4 +1,5 @@
-<h1> Welcome! </h1>
-<p> You are currently logged in as <strong> <?php echo $this->session->userdata('username'); ?> </strong> with a role of <strong> <?php echo ucfirst($this->session->userdata('active_role')); ?> </strong> </p>
-<hr>
-<?php $this->load->view('admin/_notifications'); ?>
+<h1>Welcome!</h1>
+<p>
+  You are currently logged in as <strong><?php echo username(); ?>
+  </strong> with a role of <strong><?php echo ucfirst(role()); ?></strong>
+</p>
