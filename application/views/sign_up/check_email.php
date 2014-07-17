f10afd892f8c7d60b75329f69c6ed1a13846298e
nmcalabroso <nmcalabroso@up.edu.ph>
2014-07-18 06:10:51 +0800

    Fixes minor error in signup.

--- application/views/sign_up/check_email.php
+++ application/views/sign_up/check_email.php
@@ -1,2 +1,2 @@
 <p>Check your email to activate your account!</p>
-</br><a href = "<?php echo site_url('home'); ?>"> Login </a><br/>
+<p><a href = "<?php echo site_url('home'); ?>">Login</a></p>
--- application/views/sign_up/invalid_email.php
+++ application/views/sign_up/invalid_email.php
@@ -1,3 +1,3 @@
 <h2>Oops!</h2>
 <p>It seems we can't validate your email. Please try again.</p>
-</br><a href = "<?php echo site_url('login'); ?>"> Login! </a><br/>
+<p><a href="<?php echo site_url('login'); ?>">Login!</a></p>
