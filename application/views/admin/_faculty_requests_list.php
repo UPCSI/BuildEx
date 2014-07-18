4bca9949daca0040af372483edf8ed927491a1d7
nmcalabroso <nmcalabroso@up.edu.ph>
2014-07-19 01:48:08 +0800

    Fixes error in accepting and rejecting faculty in admin.

--- application/views/admin/_faculty_requests_list.php
+++ application/views/admin/_faculty_requests_list.php
@@ -1,45 +1,41 @@
 <h3> Faculty Requests 
-    <?php if(isset($requests)): ?>
-        (<?php echo count($requests); ?>)
-    <?php endif; ?>
+  <?php if(isset($requests)): ?>
+    (<?php echo count($requests); ?>)
+  <?php endif; ?>
 </h3>
 <?php if(isset($requests)): ?>
-    <table>
-        <thead>
-            <tr>
-                <th width="200">Name</th>
-                <th width="150">Username</th>
-                <th width="150">Faculty No.</th>
-                <th width="150">Joined</th>
-                <th width="200">Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach($requests as $member): ?>
-            <tr>
-                <td>
-                    <a href = "<?php echo site_url('faculty/view/'.$member->username); ?>">
-                        <?php echo format_full_name($member); ?>
-                    </a>
-                </td>
-                <td><?php echo $member->username; ?></td>
-                <td><?php echo $member->faculty_num; ?></td>
-                <td> mm-dd-yyyy </td>
-                <td>
-                    <?php echo form_open('admin/confirm_faculty'); ?>
-                        <?php echo form_hidden('faculty_id', $member->fid); ?>
-                        <button class = "button tiny" type="submit" value="confirm">&#x2713;</button>
-                    <?php echo form_close(); ?>
+  <table>
+    <thead>
+      <tr>
+        <th width="200">Name</th>
+        <th width="150">Username</th>
+        <th width="150">Faculty No.</th>
+        <th width="150">Joined</th>
+        <th width="200">Actions</th>
+      </tr>
+    </thead>
+      <tbody>
+        <?php foreach($requests as $member): ?>
+        <tr>
+          <td><?php echo anchor(faculty_path($member), format_full_name($member)); ?></td>
+          <td><?php echo $member->username; ?></td>
+          <td><?php echo $member->faculty_num; ?></td>
+          <td>mm-dd-yyyy</td>
+          <td>
+            <?php echo form_open('admins/confirm_faculty'); ?>
+              <?php echo form_hidden('faculty_id', $member->fid); ?>
+              <button class = "button tiny" type="submit" value="confirm">&#x2713;</button>
+            <?php echo form_close(); ?>
 
-                    <?php echo form_open('admin/reject_faculty'); ?>
-                        <?php echo form_hidden('faculty_id', $member->fid); ?>
-                        <button class = "button tiny" type="submit" value="reject">&#x2717;</button>
-                    <?php echo form_close(); ?>
-                </td> 
-            </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
+            <?php echo form_open('admins/reject_faculty'); ?>
+              <?php echo form_hidden('faculty_id', $member->fid); ?>
+              <button class = "button tiny" type="submit" value="reject">&#x2717;</button>
+            <?php echo form_close(); ?>
+          </td> 
+        </tr>
+        <?php endforeach; ?>
+      </tbody>
+  </table>
 <?php else: ?>
-    </p> There are no pending requests.</p>
+  </p> There are no pending requests.</p>
 <?php endif; ?>
