c4c1e8d070d1d6ca0122cb0d83807f05e9c8d060
erbunao <earle.bunao@gmail.com>
2014-07-19 14:38:54 +0800

    Hides download link if respondents count is 0

--- application/views/experiment/view.php
+++ application/views/experiment/view.php
@@ -9,10 +9,12 @@
 		<p class = "white"> <strong> Current Count: </strong><?php echo $experiment->current_count; ?> (<a href = "<?php echo site_url('experiment/respondents/'.$experiment->eid); ?>"> View All </a> ) </p>
 		<p class = "white"> <strong> Status: </strong><?php if($experiment->status == 'f'){echo 'On-going';}else{echo 'Complete';} ?> </p>
 		<p class = "white"> <strong> Is published: </strong><?php if($experiment->is_published == 'f'){echo 'False';}else{echo 'True';} ?> </p>
-		<p class = "white"><strong> Respondent List (CSV): </strong><?php echo anchor(experiment_path($researcher, $experiment, 'download'), 'Download Link Here'); ?></p>
 		<?php if($experiment->is_published == 'f'): ?>
 			<?php echo anchor(experiment_path($researcher, $experiment, 'publish'), 'Publish', 'class = "button small"'); ?>
 		<?php else: ?>
+			<?php if($experiment->current_count > 0): ?>
+				<p class = "white"><strong> Respondent List (CSV): </strong><?php echo anchor(experiment_path($researcher, $experiment, 'download'), 'Download Link Here'); ?></p>
+			<?php endif; ?>
 			<p class = "white"><strong>URL:</strong><?php echo anchor(respond_path($experiment)); ?></p>
 			<?php echo anchor(experiment_path($researcher, $experiment, 'unpublish'), 'Unpublish', 'class = "button small"'); ?>
 		<?php endif; ?>
