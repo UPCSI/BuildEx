2b0f13ba33878e6ded0cbd065c560677cdf80cb0
erbunao <earle.bunao@gmail.com>
2014-07-19 14:30:58 +0800

    Site_url in js

--- application/views/includes/javascripts/respondent.php
+++ application/views/includes/javascripts/respondent.php
@@ -1,9 +1,1 @@
-<script>
-  function js_site_url(){
-    return urlTmp = "<?php echo site_url(); ?>";
-  }
-  function js_base_url(){
-    return urlTmp = "<?php echo base_url(); ?>";
-  }
-</script>
 <script src="<?php echo site_url('js/respondents.js'); ?>"></script>
--- application/views/main_layout.php
+++ application/views/main_layout.php
@@ -20,4 +20,12 @@
     <?php $this->load->view($main_content); ?>
 </body>
     <?php $this->load->view('includes/javascripts/foundation'); ?>
+<script>
+  function js_site_url(){
+    return urlTmp = "<?php echo site_url(); ?>";
+  }
+  function js_base_url(){
+    return urlTmp = "<?php echo base_url(); ?>";
+  }
+</script>
 </html>
