e5f81144d881008051341d074d1b0a66f6bbb105
erbunao <earle.bunao@gmail.com>
2014-07-19 15:03:29 +0800

    Goodbye page tweak

--- application/views/respondent/leave.php
+++ application/views/respondent/leave.php
@@ -1,6 +1,6 @@
 <div class="main-body" style="min-height:80%">
 	<div class="row" style="text-align:center; padding-top:100px">
-		<h1> Good bye! </h1>
-		<h2> Come again </h2>
+		<h1 class="white"> Good bye! </h1>
+		<h2 class="white"> Come again </h2>
 	</div>
 </div>
